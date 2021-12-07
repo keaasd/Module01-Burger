a1844830775872092dc9c9934d543f24bb61b71a
“Евгений <kea-asd@yandex.ru>
2021-12-08 01:02:33 +0700

    button up

---
+++ js/main.js
@@ -0,0 +1,42 @@
+// Smooth Scroll
+const links = document.querySelectorAll(".banner-link");
+
+    for (const link of links) {
+    link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+    e.preventDefault();
+    const href = this.getAttribute("href");
+
+document.querySelector(href).scrollIntoView({
+    behavior: "smooth"
+});
+}
+
+// Button Up
+
+       window.onscroll = function(){scrollFunction()};
+
+       const upbuttons = document.querySelectorAll(".button-up");
+
+       for (const upbutton of upbuttons) {
+           upbutton.addEventListener("click", clickHandler);
+       }
+
+       function clickHandler(e) {
+        e.preventDefault();
+        const href = this.getAttribute("href");
+
+       document.querySelector(href).scrollIntoView({
+           behavior: "smooth"
+       });
+       }
+
+       function scrollFunction(){
+           if ( document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000){
+               document.getElementById('btnUp').className = 'button-up visible';
+           } else {
+               document.getElementById('btnUp').className = 'button-up hidden';
+           }
+       }
