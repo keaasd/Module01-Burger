83e58f97920348dad74d2844ec6f59490fe0c020
“Евгений <kea-asd@yandex.ru>
2021-12-26 20:28:14 +0700

    add files

---
+++ js/modal.js
@@ -0,0 +1,10 @@
+// Menu
+
+function openModal() {
+    document.getElementById("modal").style.top = "0px";
+    }
+
+function closeModal() {
+    document.getElementById("modal").style.top = "-400px";
+    }
+
