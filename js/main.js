af4b1bb002a8801db01592d1b5159283beb05090
“Евгений <kea-asd@yandex.ru>
2022-01-06 01:26:23 +0700

    home page fork

--- js/main.js
+++ js/main.js
@@ -16,27 +16,27 @@ document.querySelector(href).scrollIntoView({
 
 // Button Up
 
-       window.onscroll = function(){scrollFunction()};
+    window.onscroll = function(){scrollFunction()};
 
-       const upbuttons = document.querySelectorAll(".button-up");
+    const upbuttons = document.querySelectorAll(".button-up");
 
-       for (const upbutton of upbuttons) {
-           upbutton.addEventListener("click", clickHandler);
-       }
+    for (const upbutton of upbuttons) {
+        upbutton.addEventListener("click", clickHandler);
+    }
 
-       function clickHandler(e) {
+    function clickHandler(e) {
         e.preventDefault();
         const href = this.getAttribute("href");
 
-       document.querySelector(href).scrollIntoView({
-           behavior: "smooth"
-       });
-       }
+    document.querySelector(href).scrollIntoView({
+        behavior: "smooth"
+    });
+    }
 
-       function scrollFunction(){
-           if ( document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000){
-               document.getElementById('btnUp').className = 'button-up visible';
-           } else {
-               document.getElementById('btnUp').className = 'button-up hidden';
-           }
-       }
+    function scrollFunction(){
+        if ( document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000){
+            document.getElementById('btnUp').className = 'button-up visible';
+        } else {
+            document.getElementById('btnUp').className = 'button-up hidden';
+        }
+    }
